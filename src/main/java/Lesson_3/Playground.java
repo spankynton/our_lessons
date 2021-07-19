91af6cf07afad03fb6abaf445f3f78508fe1cde4
spankynton <uskov.v.v@hotmail.com>
2021-07-19 13:59:46 +0300

    init_commit

---
+++ src/main/java/Lesson_3/Playground.java
@@ -0,0 +1,23 @@
+package Lesson_3;
+
+import java.util.Scanner;
+
+import static java.lang.System.*;
+
+/**
+ * Класс создан с целью самостоятельного написания кода в рамках текущего урока
+ */
+public class Playground {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 20; i++) {
+            if (i % 2 == 0) continue;
+            out.println(i);
+            // код
+        }
+
+    }
+
+
+}
